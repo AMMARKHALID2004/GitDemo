1fc7e8ee41cba17a6d60121233a1beb025998969
AMMARKHALID2004 <ammarkhalidseptember62@gmail.com>
2025-03-13 00:04:19 +0500

    branch test

--- UserInfo.java
+++ UserInfo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class UserInfo{
 	static String Email="FA24-BCS-137@cuilahore.edu.pk";
-	static String Password="Cui@lahore";
+	static String Password="Cui@lahore2024";
 
 	public static void main(String args[]){
 	Scanner sc=new Scanner(System.in);
